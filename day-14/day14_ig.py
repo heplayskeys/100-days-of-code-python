967e97510bbcb78378e91da34c61e03bb833e5aa
heplayskeys <43631047+heplayskeys@users.noreply.github.com>
2024-05-16 14:44:23 -0700

    Fix day14 and add Day 15 - Coffee Machine (#11)

--- day-14/day14_ig.py
+++ day-14/day14_ig.py
@@ -3,31 +3,31 @@ from random import choice
 from higher_or_lower_art import logo, versus
 from game_data import data
 
-def play_round(account_1, score=0):
+def play_round(account_a, score=0):
   if score > 0:
     print(f"You're right! Current score: {score}")
 
   round_accounts = {
-    'A': account_1,
+    'A': account_a,
   }
-  show_info(account_1, 'A')
+  show_info(account_a, 'A')
   print(versus)
-  account_2 = pick_account()
+  account_b = pick_account()
 
-  while account_1 == account_2:
-    account_2 = pick_account()
+  while account_a == account_b:
+    account_b = pick_account()
 
-  round_accounts['B'] = account_2
-  show_info(account_2, 'B')
+  round_accounts['B'] = account_b
+  show_info(account_b, 'B')
   guess = make_guess()
     
-  if guess_correct(account_1['follower_count'], account_2['follower_count'], guess):
+  if guess_correct(account_a['follower_count'], account_b['follower_count'], guess):
       score += 1
       os.system("clear")
       print(logo)
       return play_round(round_accounts[guess], score)
   else:
-    correct_answer = account_2 if guess == "A" else account_1
+    correct_answer = account_b if guess == "A" else account_a
     print(f"WRONG! It was {correct_answer['name']} with {correct_answer['follower_count']} million followers.")
     print(f"You scored {score} {'points' if score > 1 else 'point'}.")
   return start()
@@ -36,17 +36,16 @@ def pick_account():
   return choice(data)
 
 def show_info(account, ltr):
-  print(f"Compare {ltr}: {account['name']}, a {account['description']}, from {account['country']}.")
+  print(f"{'Compare' if ltr == 'A' else 'Against'} {ltr}: {account['name']}, a {account['description']}, from {account['country']}.")
 
 def make_guess():
   return input("Who has more followers? Type 'A' or 'B': ").upper()
 
-def guess_correct(account_1_followers, account_2_followers, guess):
-  if (account_1_followers > account_2_followers) and (guess == "A"):
-    return True
-  elif (account_1_followers < account_2_followers) and (guess == "B"):
-    return True
-  return False
+def guess_correct(account_a_followers, account_b_followers, guess):
+  if (account_a_followers > account_b_followers):
+    return guess == "A"
+  else:
+    return guess == "B"
 
 def start():
   if input("Want to play 'Higer or Lower'? Type 'y' to play, hit ENTER to exit: ").lower() == 'y':
---
+++ day-15/README.md
@@ -0,0 +1,17 @@
+### Coffee Machine
+
+Interact with the terminal for a hot espresso drink. Choose an `espresso`, a `latte`, or a `cappuccino` and enjoy. If you're the maintenance person, then type `report` to view the status of the machines resources and how much money it has made while in operation. If the machine requires servicing, remember to turn it `off` first.
+
+```
+What would you like? (espresso/latte/cappuccino): espresso
+Please insert coins.
+How many quarters?: 4
+How many dimes?: 3
+How many nickels?: 2
+How many pennies?: 10
+Here is your espresso. Enjoy!
+
+What would you like? (espresso/latte/cappuccino): off
+
+Thank you! Goodbye.
+```
