6f6f030a8ce2efd807dec9e4986d8dc9337c70a3
heplayskeys <43631047+heplayskeys@users.noreply.github.com>
2024-05-15 21:24:32 -0700

    Fix higher or lower IG (#10)

--- day-14/day14_ig.py
+++ day-14/day14_ig.py
@@ -3,8 +3,8 @@ from random import choice
 from higher_or_lower_art import logo, versus
 from game_data import data
 
-def play_round(account_1, played, score=0):
-  if len(played) > 0:
+def play_round(account_1, score=0):
+  if score > 0:
     print(f"You're right! Current score: {score}")
 
   round_accounts = {
@@ -13,27 +13,19 @@ def play_round(account_1, played, score=0):
   show_info(account_1, 'A')
   print(versus)
   account_2 = pick_account()
-  played.add(account_1['name'])
 
-  while account_2['name'] in played:
+  while account_1 == account_2:
     account_2 = pick_account()
 
-  played.add(account_2['name'])
   round_accounts['B'] = account_2
   show_info(account_2, 'B')
   guess = make_guess()
-  correct_answer = guess_correct(account_1['follower_count'], account_2['follower_count'], guess)
-
-  if correct_answer:
-    score += 1
-
-    if len(played) == len(data):
-      print("\nYOU DID IT! You guessed them ALL correct! Well done!\n")
-      return
-    else:
+    
+  if guess_correct(account_1['follower_count'], account_2['follower_count'], guess):
+      score += 1
       os.system("clear")
       print(logo)
-      return play_round(round_accounts[guess], played, score)
+      return play_round(round_accounts[guess], score)
   else:
     correct_answer = account_2 if guess == "A" else account_1
     print(f"WRONG! It was {correct_answer['name']} with {correct_answer['follower_count']} million followers.")
@@ -60,7 +52,7 @@ def start():
   if input("Want to play 'Higer or Lower'? Type 'y' to play, hit ENTER to exit: ").lower() == 'y':
     os.system("clear")
     print(logo)
-    play_round(pick_account(), set())
+    play_round(pick_account())
   else:
     print("\nGoodbye")
 
